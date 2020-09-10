d613b1ee753c86412373a215b5bb4f59f4e64c56
sawa9889 <cfif.sawa@bk.ru>
2020-09-10 23:24:27 +0300

    Small refactoring

--- engine/physics_processor.lua
+++ engine/physics_processor.lua
@@ -13,10 +13,15 @@ PhysicsProcessor = Class {
     end
 }
 
-function PhysicsProcessor:addLayer( newLayer )
+function PhysicsProcessor:allLayerNames()
+	local newCollidedLayers = {}
     for ind, layer in pairs(self.layers) do
-        table.insert( layer.collidedLayers, newLayer.name )
+    	table.insert( newCollidedLayers, layer.name )
     end
+    return newCollidedLayers
+end
+
+function PhysicsProcessor:addLayer( newLayer )
     table.insert( self.layers, newLayer )
 end
 
@@ -24,39 +29,26 @@ function PhysicsProcessor:registerLayer( layerName, gravity )
 	-- Добавляет в обработчика всех колизий новый слой, с указанной гравитацией и двумя пустыми списками - CollidedLayers, ActionLayers
 	local newLayer = {	
 					    name = layerName,
-					    gravityEnabled = gravity
+					    gravityEnabled = gravity and gravity or true
 					    collidedLayers = {},
 					    actionLayers = {},
 					 }
-    for ind, layer in pairs(self.layers) do
-        table.insert( newLayer.collidedLayers, layer.name )
-    end			 
 	self:addLayer(newLayer)
 end
 
 function PhysicsProcessor:addCollidedLayers( layerName, collidedLayersNames )
-	-- Добавить для слоя слои от которых он отталкивается
     for _, name in pairs(collidedLayersNames) do
-    	local index = findIn(self.layers, name)
-        if index == -1 then
-        	-- Удалить слой из списка
-        end
-    end
-    for _, name in pairs(collidedLayersNames) do
-        table.insert( self.layers[layerName].collidedLayers, name )
+        if findIn(self.layers, name) ~= -1 and findIn( self.layers[layerName].collidedLayers, name) == -1 then
+        	table.insert( self.layers[layerName].collidedLayers, name )
+    	end
     end
 end
 
 function PhysicsProcessor:addActionLayers( layerName, actionLayersNames )
-	-- Добавить для слоя слои с которыми он взаимодействует
     for _, name in pairs(actionLayersNames) do
-    	local index = findIn(self.layers, name)
-        if index == -1 then
-        	-- Удалить слой из списка
-        end
-    end
-    for _, name in pairs(actionLayersNames) do
-        table.insert( self.layers[layerName].actionLayers, name )
+        if findIn(self.layers, name) ~= -1 and findIn( self.layers[layerName].actionLayers, name) == -1 then
+        	table.insert( self.layers[layerName].actionLayers, name )
+    	end
     end
 end
 
@@ -83,17 +75,22 @@ function PhysicsProcessor:registerObject( object, layer, type, linkedTo)
 	-- По сути инитится PhysicsObject в нужном объекте с указанными параметрами
 end
 
-function PhysicsProcessor:CalculateCollisions()
+function PhysicsProcessor:calculateCollisions()
 	-- Найти объект с линкованными коллайдерами и пройтись по всем разом, рассчитывая с единым дельта вектором для всех
     for ind, object in pairs(self.objects) do
+    	-- Взять все коллизии объекта
         local collisions = self.HC:collisions(object.collider)
         for shape, delta in pairs(collisions) do
+    		-- Найти объект с которым сколлизировал среди зарегистрированных
         	local collidedObject
             for ind, secondObject in pairs(self.objects) do
                 collidedObject = secondObject.collider == shape and secondObject or collideObject
             end
+
+    		-- Для найденного объекта определить слой и определить находится ли он в группах коллизирующих или взаимодействующих слоёв основного объекта
             if collidedObject then  
     			local collideIndex = findIn(self.layers[object.collider.layer].collidedLayers, collidedObject.collider.layer)
+    			-- Если с данным слоем основной объект коллизирует, используем его функцию коллизий или базовую 
   				if collideIndex > -1 and object.isColliding then
   					if object.regiterCollision then
   						object:regiterCollision(object, collideObject, delta)
@@ -102,6 +99,7 @@ function PhysicsProcessor:CalculateCollisions()
   					end
   				end
     			local actionIndex = findIn(self.layers[object.collider.layer].actionLayers, collidedObject.collider.layer)
+    			-- Если с данным слоем основной объект взаимодействует, используем его функцию ответочки на взаимодействие
   				if actionIndex > -1 then
   					if object.regiterAction then
                 		object:regiterAction(object, collideObject, delta)
@@ -118,7 +116,7 @@ function PhysicsProcessor:update( dt )
 	-- Модуль просчёта взаимодействия объектов
 	-- Модуль движения всех объектов зарегистрированных в игре
 	-- Модуль действий с анимацией
-	self:CalculateCollisions()
+	self:calculateCollisions()
 	MovingProcessor:update( dt )
 end
 
